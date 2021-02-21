2cf016dc511618e04f4a41494775dba7ffc7672c
LiuLinFan <69246786+LiuLinFan@users.noreply.github.com>
2021-02-22 01:30:21 +0800

    mod

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
 
+func main() {
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println("hello")
+			time.Sleep(time.Second)
+		}
+	}()
+
+	http.ListenAndServe(":8080")
 }
